963ab30736eb7a684138b3fc5b120227432019ae
choimory <choimory@naver.com>
2024-12-10 22:14:23 +0900

    fix: 모듈 등록

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -3,10 +3,11 @@ import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
+import { MemberSuspension } from './entities/member-suspension.entity';
 
 @Module({
   controllers: [MemberController],
   providers: [MemberService],
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member, MemberSuspension])],
 })
 export class MemberModule {}
