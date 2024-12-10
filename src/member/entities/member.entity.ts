e2e3fe6170d0051596bd2619a7d8bd888e17afdf
choimory <choimory@naver.com>
2024-12-10 21:27:58 +0900

    feat: member entity

--- src/member/entities/member.entity.ts
+++ src/member/entities/member.entity.ts
@@ -1,9 +1,10 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryColumn } from 'typeorm';
 import { CommonTime } from '../../common/entities/common-time.entity';
+import { v7 as uuid } from 'uuid';
 
 @Entity()
 export class Member extends CommonTime {
-  @PrimaryColumn()
+  @PrimaryColumn('uuid')
   id: string;
 
   @Column({ unique: true, nullable: false })
@@ -16,7 +17,7 @@ export class Member extends CommonTime {
   password: string;
 
   @Column({ nullable: true })
-  profileImage: string;
+  image: string;
 
   @Column({ nullable: true })
   introduce: string;
@@ -29,7 +30,7 @@ export class Member extends CommonTime {
     email: string,
     nickname: string,
     password: string,
-    profileImage: string,
+    image: string,
     introduce: string,
   ) {
     super(createdAt, modifiedAt, deletedAt);
@@ -37,7 +38,12 @@ export class Member extends CommonTime {
     this.email = email;
     this.nickname = nickname;
     this.password = password;
-    this.profileImage = profileImage;
+    this.image = image;
     this.introduce = introduce;
+  }
+
+  @BeforeInsert()
+  beforeInsert() {
+    this.id = uuid();
   }
 }
