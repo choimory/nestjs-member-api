549dec8a81dfc45d4f7b393d78e9e41a8a8c7e8d
choimory <choimory@naver.com>
2024-12-12 03:12:49 +0900

    feat: service layer에 repository, datasource 추가

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Member } from './entities/member.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(
+    @InjectRepository(Member)
+    private readonly memberRepository: Repository<Member>,
+    private readonly dataSource: DataSource,
+  ) {}
+}
